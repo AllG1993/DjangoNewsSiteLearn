714804df4c00c155c34b3ad18175206647eb9fd8
Aleksey G <alekseygalkovich@gmail.com>
2022-05-23 10:54:15 +0300

    Switching to named links.

--- news/urls.py
+++ news/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import index, get_category
 
 urlpatterns = [
-    path('', index),
-    path('category/<int:category_id>/', get_category),
+    path('', index, name='home_url'),
+    path('category/<int:category_id>/', get_category, name='category_url'),
 ]

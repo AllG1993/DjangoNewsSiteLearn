0bc102b41e8b39a6270b72c9d82230593060dfec
Aleksey G <alekseygalkovich@gmail.com>
2022-05-23 09:42:59 +0300

    Adding a sidebar with news categories, implementing a selection by categories.

--- news/views.py
+++ news/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import News
+from .models import News, Category
 
 
 def index(request):
     news = News.objects.all()
+    categories = Category.objects.all()
     context = {
         'news': news,
-        'title': 'News list'
+        'title': 'News list',
+        'categories': categories
     }
     return render(request, 'news/index.html', context)
+
+
+def get_category(request, category_id):
+    news = News.objects.filter(category_id=category_id)
+    categories = Category.objects.all()
+    current_category = Category.objects.get(pk=category_id)
+    context = {'news': news, 'categories': categories, 'current_category': current_category}
+    return render(request, 'news/category.html', context)
+
+
+
 
